1e4b2ee30f30a6a891e64db0d785d2e4e03f84c8
ghanekarparnika <parnikaghanekar@gmail.com>
2023-08-31 18:00:25 +0530

    Parnika:Select EmpHrs by using switch case statements and calculate wage

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -4,41 +4,40 @@
 //Use Case 1-EmployeeAttendance
 //Use Case 2- Calculate Epmloyee Daily Wage
 //Use Case 3-Adding Part_Time_Employee and calculating Wage 
+//Use Case 4-Solving using switch case statement
 using System;
 namespace Employee_Wage
 {
     internal class Program 
     {
+        //defining const here
+        const int Is_Full_Time = 1;
+        const int Is_Part_Time = 2;
+        const int Emp_Rate_Per_Hr = 20;
         static void Main(string[] args) 
         {
-            //defining const here
-            int Is_Full_Time = 1;
-            int Is_Part_Time = 2;
-            int Emp_Rate_Per_Hr = 20;
             //variables
             int empHrs = 0, empWage = 0;
             //creating object of Randaom class
             Random rnd = new Random();
             int empCheck=rnd.Next(0,3);
             Console.WriteLine(empCheck);
-           
-
-            if (empCheck == Is_Full_Time)
+            switch(empCheck)
             {
-                Console.WriteLine("Full Time Employee");
-                empHrs = 8;
+                case Is_Full_Time:
+                    Console.WriteLine("Full time Employee");
+                    empHrs = 8;
+                    break;
+                case Is_Part_Time:
+                    Console.WriteLine("Part time Employee");
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            
             }
-            else if (empCheck== Is_Part_Time) 
-            {
-                Console.WriteLine("Part Time Employee");
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
-            }
-               
-
+          
             empWage = empHrs * Emp_Rate_Per_Hr;
             Console.WriteLine("Employee Wage: "+empWage);
 
