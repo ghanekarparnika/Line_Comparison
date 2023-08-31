2d2b4a3df0cc2f066fb3a22a35f3cae4d8c7a3fd
ghanekarparnika <parnikaghanekar@gmail.com>
2023-08-31 15:06:35 +0530

    [parnika]:Adding parttime employee and calculating wage for employee according hours

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -3,6 +3,7 @@
 
 //Use Case 1-EmployeeAttendance
 //Use Case 2- Calculate Epmloyee Daily Wage
+//Use Case 3-Adding Part_Time_Employee and calculating Wage 
 using System;
 namespace Employee_Wage
 {
@@ -12,26 +13,31 @@ namespace Employee_Wage
         {
             //defining const here
             int Is_Full_Time = 1;
+            int Is_Part_Time = 2;
             int Emp_Rate_Per_Hr = 20;
             //variables
             int empHrs = 0, empWage = 0;
             //creating object of Randaom class
             Random rnd = new Random();
-            int empCheck=rnd.Next(0,2);
+            int empCheck=rnd.Next(0,3);
             Console.WriteLine(empCheck);
-            /* if (empCheck == Is_Full_Time)
-             {
-                 Console.WriteLine("Employee is present");
-             }
-             else
-                 Console.WriteLine("Employee is absent");*/
+           
 
             if (empCheck == Is_Full_Time)
             {
+                Console.WriteLine("Full Time Employee");
                 empHrs = 8;
             }
+            else if (empCheck== Is_Part_Time) 
+            {
+                Console.WriteLine("Part Time Employee");
+                empHrs = 4;
+            }
             else
-                empHrs=0;
+            {
+                empHrs = 0;
+            }
+               
 
             empWage = empHrs * Emp_Rate_Per_Hr;
             Console.WriteLine("Employee Wage: "+empWage);
