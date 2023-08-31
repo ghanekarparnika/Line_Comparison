0cd65425cd6050ecc629efd34b4eaca31886456f
ghanekarparnika <parnikaghanekar@gmail.com>
2023-08-31 13:03:59 +0530

    [parnika]:Calculating employee wage as per employee rate per hour:20

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -2,6 +2,7 @@
 //Console.WriteLine("Hello, World!");
 
 //Use Case 1-EmployeeAttendance
+//Use Case 2- Calculate Epmloyee Daily Wage
 using System;
 namespace Employee_Wage
 {
@@ -11,16 +12,29 @@ namespace Employee_Wage
         {
             //defining const here
             int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hr = 20;
+            //variables
+            int empHrs = 0, empWage = 0;
             //creating object of Randaom class
             Random rnd = new Random();
             int empCheck=rnd.Next(0,2);
             Console.WriteLine(empCheck);
+            /* if (empCheck == Is_Full_Time)
+             {
+                 Console.WriteLine("Employee is present");
+             }
+             else
+                 Console.WriteLine("Employee is absent");*/
+
             if (empCheck == Is_Full_Time)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
-                Console.WriteLine("Employee is absent");
+                empHrs=0;
+
+            empWage = empHrs * Emp_Rate_Per_Hr;
+            Console.WriteLine("Employee Wage: "+empWage);
 
 
         }
