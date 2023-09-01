3eda816b959f808a9a0d23bf1c83a568cdda6a35
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-01 17:55:32 +0530

    [parnika]:Checking eqality of two lines based on the end points

--- Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Program.cs
@@ -8,10 +8,28 @@
 
 internal class Program 
 {
-    static double CalculateLength(double x1, double y1, double x2, double y2)
+    static double CalculateLength1(double x1, double y1, double x2, double y2)
     {
-        double length= Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 -y1, 2));
-        return length;
+        double length1= Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 -y1, 2));
+        return length1;
+
+    }
+
+    static double CalculateLength2(double p1, double k1, double p2, double k2)
+    {
+        double length2 = Math.Sqrt(Math.Pow(p2 - p1, 2) + Math.Pow(k2 - k1, 2));
+        return length2;
+
+    }
+
+    static void Is_Equal(double l1, double l2)
+    {
+        if (l1 == l2)
+        {
+            Console.WriteLine("Two Lines are equal");
+        }
+        else
+            Console.WriteLine("Lines are not Equal");
 
     }
     static void Main(string[] args)
@@ -21,12 +39,20 @@ internal class Program
         double y1 = random.Next(0,9);
         double x2 = random.Next(0,9);
         double y2 = random.Next(0,9);
-        //double x1=1,x2=2,y1=3,y2=6;
-        Console.WriteLine(x1);
-        Console.WriteLine(y1);
-        Console.WriteLine(x2);
-        Console.WriteLine(y2);
-        double length = CalculateLength(x1, y1, x2, y2);
-        Console.WriteLine("Length of line:"+length);
+
+
+        double p1 = random.Next(0, 9);
+        double k1 = random.Next(0, 9);
+        double p2 = random.Next(0, 9);
+        double k2 = random.Next(0, 9);
+
+        //double x1 = 1, x2 = 2, y1 = 3 ,y2 = 6;
+        //double p1 = 1, p2 = 2, k1 = 3, k2 = 6;
+
+        double length1 = CalculateLength1(x1, y1, x2, y2);
+        double length2 = CalculateLength2(p1, k1, p2, k2);
+        Console.WriteLine("Length of line1:"+length1+"      "+ "Length of line1:" + length2);
+        Is_Equal(length1, length2);
+        
     }
 }
